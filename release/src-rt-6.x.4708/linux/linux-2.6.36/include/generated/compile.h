3279d6b52f6795348e4a9e461ab10b92905d64ea
Eric Sauvageau <merlin@asuswrt-merlin.net>
2020-10-30 23:22:19 -0400

    Merge HND5.02 + RT-AC86U binary blobs (from 386_40577)

---
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Thu Oct 29 11:43:53 CST 2020"
+#define LINUX_COMPILE_TIME "11:43:53"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "asus"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.5.3 (Buildroot 2012.02) "
